6027c92535799b3f134ff2644dee76e36f6de19d
gustavoaso <gustavo.alcantara@aluno.ifsp.edu.br>
2026-04-13 08:30:41 -0300

    codigação

---
+++ prisma/migrations/20260413102808_create_field_role_table_user/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `user` DROP COLUMN `password`,
+    ADD COLUMN `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER';
