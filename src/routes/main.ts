24c22dd51aef1f487d8bd8faeb9ed4af44a0a919
gustavoaso <gustavo.alcantara@aluno.ifsp.edu.br>
2026-05-04 08:37:16 -0300

    Modificando criação de usuário

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { prisma } from '../libs/prisma';
+import { createUser } from '../services/user';
 
 export const mainRouter = Router();
 
@@ -7,12 +8,10 @@ mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
 
-mainRouter.post('/user', async (req, res) =>{
-    const user = await prisma.user.create({
-        data: {
+mainRouter.post('/user', async (req, res) => {
+    const user = await createUser({
             name: "John Doe",
             email: "john.doe@exemple.com",
-        }
     })
 
     res.json(user)
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,16 +1,19 @@
 import { Prisma } from "@prisma/client"
 import { prisma } from "../libs/prisma"
-import { createUser } from "../services/user";
 
-export const mainRouter = Router();
+type CreateUserProps = {
+  name: string
+  email: string
+}
 
-mainRouter.get('/ping', (req, res) => {
-    res.json({ pong: true });
-});
+export const createUser = async ({ name, email }: 
+CreateUserProps) => {
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email
+    }
+  })
 
-mainRouter.post('/user', async (req, res) =>
-    const user = await createUser(
-        "jonh Doe",
-        "john.doe@exemploo.com"
-    )
-)
+  return user
+}
