ed15f89124cdc66080118b181cc4f97fd6c1fa97
gustavoaso <gustavo.alcantara@aluno.ifsp.edu.br>
2026-05-11 08:34:55 -0300

    criando vários usuários

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { prisma } from '../libs/prisma';
-import { createUser } from '../services/user';
+import { createUser, createUsers } from '../services/user';
 
 export const mainRouter = Router();
 
@@ -10,15 +10,27 @@ mainRouter.get('/ping', (req, res) => {
 
 mainRouter.post('/user', async (req, res) => {
     const user = await createUser({
-            name: "John Doe",
-            email: "john.doe@exemple.com",
-    });
+        name: "John Doe",
+        email: "john.doe@exemple.com",
+    })
 
     if (user) {
         res.status(201).json(user);
     } else {
         res.status(400).json({ error: 'Email already exists' });
     }
+});
 
-    res.json(user)
+mainRouter.post('/users', async (req, res) => {
+    const result = await createUsers([
+        {name: "Alice Smith", email: "alice.smith@example.com"},
+        {name: "Bob Johnson", email: "bob.johnson@example.com"},
+        {name: "Charlie Brown", email: "charlie.brown@example.com"},
+        {name: "David Wilson", email: "david.wilson@example.com"}
+    ])
+    if (result) {
+        res.status(201).json({ ok: true });
+    } else {
+        res.status(400).json({ error: 'Error creating users' });
+    }
 })
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,22 +1,20 @@
 import { Prisma } from "@prisma/client"
 import { prisma } from "../libs/prisma"
-
+/*
 type CreateUserProps = {
   name: string
   email: string
 }
-
-export const createUser = async ({ name, email }:
-  CreateUserProps) => {
+*/
+export const createUser = async (data: Prisma.UserCreateInput) => {
   try {
-    const user = await prisma.user.create({
-      data: {
-        name,
-        email
-      }
+   /* const user = await prisma.user.create({
+      data
     })
 
     return user
+    */
+   return await prisma.user.create({ data })
   } catch (error) {
 
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
@@ -25,5 +23,18 @@ export const createUser = async ({ name, email }:
         return false
       }
     }
+    console.error('Error creating user:', error)
+  }
+}
+
+export const createUsers = async (users: Prisma.UserCreateInput[]) => { 
+  try {
+    return await prisma.user.createMany({
+      data: users,
+      skipDuplicates: true
+    })
+  } catch (error) {
+    console.error('Error creating users:', error)
+    return false
   }
 }
